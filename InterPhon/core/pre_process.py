b09e6c36ba7e5bc6af24404a0d62978af80f4f36
In Won Yeu <yiw0121@snu.ac.kr>
2021-04-05 19:13:17 +0900

    Add files via upload

--- InterPhon/core/pre_process.py
+++ InterPhon/core/pre_process.py
@@ -128,7 +128,7 @@ class PreProcess(object):
                 _enlarge = _enlarge * self.user_arg.enlargement[ind]
 
         for i, ind_T in enumerate(self.unit_cell.atom_true):
-            _dis_super_cell.atom_cart = self.super_cell.atom_cart.copy()
+            _dis_super_cell.atom_cart = _current_position.copy()
             for j, displace in enumerate(np.eye(3, dtype=float)):
 
                 # Forward Displacement
