a96eb952665d874847358935e2cc669eecebc7a4
In Won Yeu <yiw0121@snu.ac.kr>
2021-06-30 12:59:45 +0900

    Add files via upload

--- InterPhon/__init__.py
+++ InterPhon/__init__.py
@@ -1,13 +1,13 @@
 """
 InterPhon: A Python Package for Ab initio Interface Phonon Calculations within a 3D Electronic Structure Framework.
 
-This package includes following five sub-packages:
+This package consists of following five sub-packages:
 
 error -> Collection of error modules defined by developer to guide users.
-inout -> Collection of parser modules to read and write files in different DFT formats.
+inout -> Collection of inout parser modules to read and write files in different DFT formats.
 core -> Collection of core modules responsible for the central processes.
 util -> Collection of util modules to extend the functionality.
-analysis -> Collection of analysis modules to characterize phonons.
+analysis -> Collection of analysis modules to characterize phonon properties.
 """
 
 __version__ = "1.4.0"
