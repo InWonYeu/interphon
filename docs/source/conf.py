641c486a9d49290db6d2dfb9ce39e8c07990aa64
In Won Yeu <yiw0121@snu.ac.kr>
2021-07-04 18:18:08 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '1.4.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-html_baseurl = 'https://github.com/InWonYeu/interphon/'
+# html_baseurl = 'https://github.com/InWonYeu/interphon/'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages', ]
 
 # Add any paths that contain templates here, relative to this directory.
