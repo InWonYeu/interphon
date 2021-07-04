876211f1d68eb1bace457bf5f3a847e3552b41b3
In Won Yeu <yiw0121@snu.ac.kr>
2021-07-04 18:26:53 +0900

    Add files via upload

--- InterPhon/analysis/band.py
+++ InterPhon/analysis/band.py
@@ -1,3 +1,13 @@
+"""
+InterPhon analysis sub-package.
+
+This sub-package consists of following four modules:
+
+dos.py -> dos class to analyze density of states.
+band.py -> band to analyze dispersion relation.
+mode.py -> mode class to analyze vibrational motions.
+thermal_properties.py -> thermal property class to analyze thermodynamic properties such as vibrational entropy.
+"""
 import numpy as np
 
 
