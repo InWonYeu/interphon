b31ccc158a0d8823455356749dcdb264dd7b6fcd
In Won Yeu <yiw0121@snu.ac.kr>
2021-08-12 20:00:36 +0900

    Update post_check.py

--- InterPhon/core/post_check.py
+++ InterPhon/core/post_check.py
@@ -1,8 +1,8 @@
 import numpy as np
 from InterPhon import error
-from .pre_check import PreArgument
-from .unit_cell import UnitCell
-from .super_cell import SuperCell
+from InterPhon.core.pre_check import PreArgument
+from InterPhon.core.unit_cell import UnitCell
+from InterPhon.core.super_cell import SuperCell
 
 
 class PostArgument(PreArgument):
